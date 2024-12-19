21bf7411669b570d50d10f8179d709b15a54b04f
Nikhilsai16802 <nikhilsai16802@gmail.com>
2024-12-19 19:03:07 +0530

    Add files via upload

---
+++ DE coding challenges/SQL coding challenge/coding challenge documentation.sql
@@ -0,0 +1,106 @@
+use coding_challenge
+
+CREATE TABLE burger_names(
+   burger_id   INTEGER  NOT NULL PRIMARY KEY 
+  ,burger_name VARCHAR(10) NOT NULL
+);
+INSERT INTO burger_names(burger_id,burger_name) VALUES (1,'Meatlovers');
+INSERT INTO burger_names(burger_id,burger_name) VALUES (2,'Vegetarian');
+
+CREATE TABLE runner_orders(
+  order_id     INTEGER  NOT NULL PRIMARY KEY,
+  runner_id    INTEGER  NOT NULL,
+  pickup_time  datetime,
+  distance     VARCHAR(7),
+  duration     VARCHAR(10),
+  cancellation VARCHAR(23),
+);
+INSERT INTO runner_orders VALUES (1,1,'2021-01-01 18:15:34','20km','32 minutes',NULL);
+INSERT INTO runner_orders VALUES (2,1,'2021-01-01 19:10:54','20km','27 minutes',NULL);
+INSERT INTO runner_orders VALUES (3,1,'2021-01-03 00:12:37','13.4km','20 mins',NULL);
+INSERT INTO runner_orders VALUES (4,2,'2021-01-04 13:53:03','23.4','40',NULL);
+INSERT INTO runner_orders VALUES (5,3,'2021-01-08 21:10:57','10','15',NULL);
+INSERT INTO runner_orders VALUES (6,3,NULL,NULL,NULL,'Restaurant Cancellation');
+INSERT INTO runner_orders VALUES (7,2,'2021-01-08 21:30:45','25km','25mins',NULL);
+INSERT INTO runner_orders VALUES (8,2,'2021-01-10 00:15:02','23.4 km','15 minute',NULL);
+INSERT INTO runner_orders VALUES (9,2,NULL,NULL,NULL,'Customer Cancellation');
+INSERT INTO runner_orders VALUES (10,1,'2021-01-11 18:50:20','10km','10minutes',NULL);
+select * from runner_orders
+
+CREATE TABLE burger_runner(
+   runner_id   INTEGER  NOT NULL PRIMARY KEY 
+  ,registration_date date NOT NULL
+);
+INSERT INTO burger_runner VALUES (1,'2021-01-01');
+INSERT INTO burger_runner VALUES (2,'2021-01-03');
+INSERT INTO burger_runner VALUES (3,'2021-01-08');
+INSERT INTO burger_runner VALUES (4,'2021-01-15');
+
+CREATE TABLE customer_orders(
+   order_id    INTEGER  NOT NULL 
+  ,customer_id INTEGER  NOT NULL
+  ,burger_id    INTEGER  NOT NULL
+  ,exclusions  VARCHAR(4)
+  ,extras      VARCHAR(4)
+  ,order_time  datetime NOT NULL
+);
+INSERT INTO customer_orders VALUES (1,101,1,NULL,NULL,'2021-01-01 18:05:02');
+INSERT INTO customer_orders VALUES (2,101,1,NULL,NULL,'2021-01-01 19:00:52');
+INSERT INTO customer_orders VALUES (3,102,1,NULL,NULL,'2021-01-02 23:51:23');
+INSERT INTO customer_orders VALUES (3,102,2,NULL,NULL,'2021-01-02 23:51:23');
+INSERT INTO customer_orders VALUES (4,103,1,'4',NULL,'2021-01-04 13:23:46');
+INSERT INTO customer_orders VALUES (4,103,1,'4',NULL,'2021-01-04 13:23:46');
+INSERT INTO customer_orders VALUES (4,103,2,'4',NULL,'2021-01-04 13:23:46');
+INSERT INTO customer_orders VALUES (5,104,1,NULL,'1','2021-01-08 21:00:29');
+INSERT INTO customer_orders VALUES (6,101,2,NULL,NULL,'2021-01-08 21:03:13');
+INSERT INTO customer_orders VALUES (7,105,2,NULL,'1','2021-01-08 21:20:29');
+INSERT INTO customer_orders VALUES (8,102,1,NULL,NULL,'2021-01-09 23:54:33');
+INSERT INTO customer_orders VALUES (9,103,1,'4','1, 5','2021-01-10 11:22:59');
+INSERT INTO customer_orders VALUES (10,104,1,NULL,NULL,'2021-01-11 18:34:49');
+INSERT INTO customer_orders VALUES (10,104,1,'2, 6','1, 4','2021-01-11 18:34:49');
+
+select * from burger_runner
+
+--1.Querying Data by Using Joins and Subqueries & subtotal
+
+SELECT customer_id, COUNT(order_id) AS total_orders
+FROM customer_orders
+GROUP BY customer_id;
+
+SELECT DISTINCT r.runner_id, r.registration_date
+FROM burger_runner r
+JOIN runner_orders ro ON r.runner_id = ro.runner_id
+WHERE ro.cancellation IS NULL;
+
+SELECT co.customer_id, co.order_id, co.order_time
+FROM customer_orders co
+WHERE co.order_time = (
+    SELECT MAX(order_time) FROM customer_orders WHERE customer_id = co.customer_id);
+
+SELECT DISTINCT co.customer_id
+FROM customer_orders co
+JOIN burger_names bn ON co.burger_id = bn.burger_id
+WHERE bn.burger_name = 'Meatlovers';
+
+SELECT customer_id, MAX(order_time) AS latest_order
+FROM customer_orders
+GROUP BY customer_id;
+
+
+--2.Manipulate data by using sql commands using groupby and having clause.
+
+SELECT customer_id, COUNT(order_id) AS total_orders
+FROM customer_orders
+GROUP BY customer_id
+HAVING COUNT(order_id) >= 3;
+
+SELECT runner_id, COUNT(order_id) AS successful_orders
+FROM runner_orders
+WHERE cancellation IS NULL
+GROUP BY runner_id
+HAVING COUNT(order_id) > 2;
+
+
+
+
+
